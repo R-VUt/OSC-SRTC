f092dfa79f8f686ad040f366f1f2a10899282e12
Soumt <soumt@dimigo.hs.kr>
2023-03-27 07:54:10 +0900

    feat: generate default settings when cannot find

--- modules/SRTC_Utils.py
+++ modules/SRTC_Utils.py
@@ -2,6 +2,28 @@ import sys
 import os
 import json
 
+default_settings = '''
+{
+	"osc_ip" : "127.0.0.1",
+	"osc_port" : 9000,
+	
+	"osc_serv_ip" : "127.0.0.1",
+	"osc_serv_port" : 9001,
+	
+	"azure_key" : "",
+	"azure_location" : "",
+	
+	"etri_key" : "",
+	
+	"papago_id" : "",
+	"papago_secret" : ""
+}
+'''
+
+def make_default_settings():
+    with open("./api_settings.json", "w") as f:
+        f.write(default_settings)
+
 def resource_path(relative_path: str) -> str:
     try:
         base_path = sys._MEIPASS
@@ -11,6 +33,9 @@ def resource_path(relative_path: str) -> str:
 
 def load_settings() -> dict:
     settings = {}
+    if not os.path.exists("./api_settings.json"):
+        make_default_settings()
+        
     try:
         with open("./api_settings.json", "r") as f:
             settings = json.load(f)
